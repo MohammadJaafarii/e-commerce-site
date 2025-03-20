dba3109307c5d421cae1b68dd36cb858a450a2c3
MJ202218 <mohamamdjafari18132@gmail.com>
2025-03-21 01:14:29 +0330

    API Paging, Filtering, Sorting & Searching

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -20,9 +20,36 @@ namespace Core.Specifications
 
         public List<Expression<Func<T, object>>> Includes {get;} = new List<Expression<Func<T, object>>>();
 
+        public Expression<Func<T, object>> OrderBy {get; private set;}
+
+        public Expression<Func<T, object>> OrderByDescending {get; private set;}
+
+        public int Take {get; private set;}
+
+        public int Skip {get; private set;}
+
+        public bool ISPagingEnabled {get; private set;}
+
         protected void AddInclude(Expression<Func<T,object>> includeExpression)
         {
             Includes.Add(includeExpression);
+        }
+
+        protected void AddOrderBy (Expression<Func<T,object>> orderByExpression)
+        {
+            OrderBy = orderByExpression;
+        }
+        protected void AddOrderByDescending (Expression<Func<T,object>> orderByDescExpression)
+        {
+            OrderByDescending = orderByDescExpression;
+        }
+
+        protected void ApplyPaging(int skip, int take)
+        {
+            this.Skip = skip;
+            this.Take = take;
+            this.ISPagingEnabled = true;
+            
         }
 
     }

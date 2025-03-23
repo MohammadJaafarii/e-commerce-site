b1f21ec7f27ff5b2d7813b04b1792881e3687563
MJ202218 <mohamamdjafari18132@gmail.com>
2025-03-23 05:41:41 +0330

    Client - Angular Basics

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +9,21 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
-}
+  products: Product[] = [];
+
+
+  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>('https://localhost:5001/api/products?pageSize=50').subscribe({
+      next: response => this.products = response.data, // what to do next
+      error: error => console.log(error), // what to do if there is an error
+      complete: () => {
+        console.log('Request completed');
+        console.log("extra statment");
+        }
+    })
+    }
+  }
+
